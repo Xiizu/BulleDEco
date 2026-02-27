259c54519cd20f1cf7417e002065d02d182692da
Lounès Souakri <lounes.souakri@etu.univ-nantes.fr>
2026-02-27 13:44:32 +0100

    update réduction de la taille des images

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,5 @@
+import os
+from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import ContactMessage
@@ -31,4 +33,17 @@ def materiaux(request):
     return render(request, 'pages/materiaux.html')
 
 def gallery(request):
-    return render(request, 'pages/gallery.html')
+    static_img_path = os.path.join(settings.BASE_DIR, 'pages', 'static', 'pages', 'images')
+    subfolders = ['avant_apres', 'planche_ambiance', 'materiaux', 'rendu_3d']
+    images = []
+    for f in os.listdir(static_img_path):
+        if f.lower().endswith(('.jpg')):
+            images.append(f'pages/images/{f}')
+    for folder in subfolders:
+        folder_path = os.path.join(static_img_path, folder)
+        if os.path.exists(folder_path):
+            for f in os.listdir(folder_path):
+                if f.lower().endswith(('.png', '.jpg', '.jpeg', '.webp')):
+                    images.append(f'pages/images/{folder}/{f}')
+
+    return render(request, 'pages/gallery.html', {'images': images})
